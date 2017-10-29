cc072f6a729ccae6b6728fbc46e5965f8db3abbb
0xADADA <0xadada.pub@0xadada.pub>
2017-10-29 16:44:03 -0400

    ch.1, 2, 3

---
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -0,0 +1,14 @@
+defmodule RumblWeb.UserController do
+  use RumblWeb, :controller
+  alias RumblWeb.User
+
+  def index(conn, _params) do
+    users = Rumbl.Repo.all(User)
+    render(conn, "index.html", users: users)
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Rumbl.Repo.get(User, id)
+    render(conn, "show.html", user: user)
+  end
+end
