3a598950c8eac5111e8824fee4436ca83350ffd5
0xADADA <0xadada.pub@0xadada.pub>
2017-10-30 07:28:05 -0400

    ch.4 defining the user schema and migrations

---
+++ lib/rumbl/user.ex
@@ -0,0 +1,15 @@
+defmodule Rumbl.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Rumbl.User
+
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+end
--- lib/rumbl_web/models/user.ex
+++
@@ -1,3 +0,0 @@
-defmodule RumblWeb.User do
-  defstruct [:id, :name, :username, :password]
-end
