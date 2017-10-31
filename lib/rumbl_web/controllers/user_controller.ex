60f1302a0c1dd7b2ca0450f3d45b24a47bb2491d
0xADADA <0xadada.pub@0xadada.pub>
2017-10-31 08:03:08 -0400

    ch.4
    creating resources

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,6 +1,6 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
-  alias RumblWeb.User
+  alias Rumbl.User
 
   def index(conn, _params) do
     users = Rumbl.Repo.all(User)
@@ -10,5 +10,19 @@ defmodule RumblWeb.UserController do
   def show(conn, %{"id" => id}) do
     user = Rumbl.Repo.get(User, id)
     render(conn, "show.html", user: user)
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    {:ok, user} = Rumbl.Repo.insert(changeset)
+
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: user_path(conn, :index))
   end
 end
