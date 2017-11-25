97874d68d6e7e9c3e8aa1a1e897c2485700fabda
0xADADA <0xadada.pub@0xadada.pub>
2017-11-25 13:51:37 -0500

    ch.10
    Re-establish connections after broken

--- assets/js/video.js
+++ assets/js/video.js
@@ -31,6 +31,7 @@ let video = {
     });
 
     vidChannel.on('new_annotation', (resp) => {
+      vidChannel.params.last_seen_id = resp.id;
       this.renderAnnotation(msgContainer, resp);
     });
 
@@ -44,6 +45,10 @@ let video = {
 
     vidChannel.join()
       .receive('ok', resp => {
+        let ids = resp.annotations.map(ann => ann.id);
+        if(ids.length > 0) {
+          vidChannel.params.last_seen_id = Math.max(...ids);
+        }
         this.scheduleMessages(msgContainer, resp.annotations);
       })
       .receive('error', msg => console.log('join failed', msg));
